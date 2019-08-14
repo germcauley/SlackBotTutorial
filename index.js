fa48a166b80a50f4404362ad15a582b30968bed8
Gerald McAuley <geraldmcauley@gwsteam.com>
2019-08-14 13:55:05 +0100

    add gitignore, bot now workinggit add *

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const axios = require('axios')
 const dotenv = require('dotenv')
 
 dotenv.config()
+
+const bot = new SlackBot({
+    token: 'xoxb-258246538437-726161173040-mZLuO53L0EFXcYUuO8KcjTwZ',
+    name: 'SlackAppTut'
+})
+bot.on('start', () => {
+    console.log('starting bot')
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
+
+    bot.postMessageToChannel(
+        'random',
+        'Hello, I am a new bot!!',
+        params
+    );
+})
+
+bot.on('error', (err) => {
+    console.log(err);
+})
