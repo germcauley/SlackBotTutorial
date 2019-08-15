e5d994e494196028dce2965cbf0553e668286ec0
Gerald McAuley <geraldmcauley@gwsteam.com>
2019-08-15 23:02:44 +0100

    trying to get data from axios

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const SlackBot = require('slackbots');
-const axios = require('axios')
-const dotenv = require('dotenv')
+const axios = require('axios');
+const dotenv = require('dotenv');
 
 dotenv.config()
 
@@ -9,41 +9,53 @@ const bot = new SlackBot({
     name: 'SlackAppTut'
 })
 
-url = 'https://slack.com/api/users.list?token=xoxb-258246538437-726161173040-mZLuO53L0EFXcYUuO8KcjTwZ&pretty=1'
+// var url = "https://slack.com/api/users.list?token=xoxb-258246538437-726161173040-mZLuO53L0EFXcYUuO8KcjTwZ&pretty=1";
 
 
 //Gets status from slack
-function getLocStatus(url){
-    results =[]
-    axios.get(url)
-      .then(function(response) {
-          x = (response.data.members).length
-          for(var i=0; i <x; i++){
-              results.push(response.data.members[i].name) 
-              results.push(response.data.members[i].profile.status_text)
-            // console.log("User: "+ response.data.members[i].name);
-            // console.log("Status: "+response.data.members[i].profile.status_text);
-          }
-      }).catch(function(error) {
-         console.log(error.response.data);
-      });
-      return ['blah'] ;
+function getLocStatus() {
+    var url = "https://slack.com/api/users.list?token=xoxb-258246538437-726161173040-mZLuO53L0EFXcYUuO8KcjTwZ&pretty=1";
+    axios.get(url).then(function (result) {
+        
+    });
 }
+//   x = (response.data.members).length
+//   for(var i=0; i <x; i++){
+//     //   response.data.members[i].name
+//     //   response.data.members[i].profile.status_text
+//     console.log("User: "+ response.data.members[i].name);
+//     t.push("User: "+ response.data.members[i].name);
+//     // return "User: "+ response.data.members[i].name;
+//     console.log("Status: "+response.data.members[i].profile.status_text);
+//   }
 
+
+function axiosTest () {
+    var url = "https://slack.com/api/users.list?token=xoxb-258246538437-726161173040-mZLuO53L0EFXcYUuO8KcjTwZ&pretty=1";
+    var strr = [];
+       axios.get(url)
+      .then(function(response){
+             return response.data.json
+       })
+       .catch(function(error){
+              console.log(error);
+          });
+      return 
+}   
 
 
 bot.on('start', () => {
     console.log('starting bot')
     const params = {
         icon_emoji: ':robot_face:'
-        
+
     }
     console.log()
 })
 
 // Message Handler
 bot.on('message', (data) => {
-    if(data.type !== 'message') {
+    if (data.type !== 'message') {
         return;
     }
     handleMessage(data.text);
@@ -51,17 +63,14 @@ bot.on('message', (data) => {
 
 // Response Handler
 function handleMessage(message) {
-    if(message.includes('hello')) {
+    if (message.includes('hello')) {
         console.log('run sayHi')
         sayHi()
-    } else if(message.includes('help')) {
+    } else if (message.includes('help')) {
         console.log('run help')
-        
-        help()
-    } 
-    else if(message.includes('status')){
+        sayHelp()
+    } else if (message.includes('status')) {
         showStatus()
-        console.log('There are x desks')
     }
 }
 
@@ -69,18 +78,27 @@ bot.on('error', (err) => {
     console.log(err);
 })
 
-function sayHi(){
+function sayHi() {
     bot.postMessageToChannel(
         'general',
-        'Hello, there!'   
+        'Hello, there!'
     );
 }
 
-function showStatus(){
-    status = getLocStatus()
-
+function sayHelp() {
     bot.postMessageToChannel(
         'general',
-         status   
+        'How can I help you?'
+    );
+}
+
+function showStatus() {
+
+    status = axiosTest();
+
+    console.log(status);
+    bot.postMessageToChannel(
+        'general',
+        status
     );
 }
